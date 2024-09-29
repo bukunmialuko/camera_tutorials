f2e0d8586beeecc7808ea548a64ef0eff4cc373a
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2024-09-29 23:37:47 +0100

    feat: updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:camera/camera.dart';
 import 'package:camera_tutorials/navigation/router.dart';
 import 'package:flutter/material.dart';
 
+List<CameraDescription> cameras = <CameraDescription>[];
+
 Future<void> main() async {
+  // Fetch the available cameras before initializing the app.
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    // _logError(e.code, e.description);
+  }
   runApp(const MyApp());
 }
 
